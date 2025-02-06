7fe44e6511a118cd1bc15e93d76c07e40d462591
KarolaKow <karolinakowalczyk1999@gmail.com>
2025-02-06 21:06:11 +0100

    initial commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {
+  private router = inject(Router);
+
+  goToRefusal() {
+    this.router.navigate(['/no']);
+  }
+}
---
+++ src/app/components/refusal/refusal.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-refusal',
+  standalone: true,
+  imports: [],
+  templateUrl: './refusal.component.html',
+  styleUrl: './refusal.component.scss'
+})
+export class RefusalComponent {
+
+}
